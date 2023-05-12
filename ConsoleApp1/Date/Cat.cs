f87ab2b3a517cf0c71681a15fdde1e68594134dd
Alfatabate <131299392+Alfatabate@users.noreply.github.com>
2023-05-12 11:42:01 +0300

    Add files via upload

---
+++ ConsoleApp1/Date/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Date
+{
+    public class Cat
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int BreedId { set; get; }
+        public Breed Breeds { get; set; }
+    }
+}
---
+++ ConsoleApp1/Date/CatsDBDContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Date
+{
+    internal class CatsDBDContext:DbContext
+    {
+        public CatsDBDContext() : base("CatsDBDContext")
+        { }
+        public DbSet<Cat>Cats { get; set; }
+        public DbSet<Breed> Breeds { get; set; }
+    }
+}
